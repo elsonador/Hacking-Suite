51eb6454c7fc93f6566e87f90d3c69f526319757
Drarqua-Storm <drarqua@hackgr.eu>
2013-02-20 23:05:59 +0200

    Fixed Licence at __init__ files

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 """
-Copyright (c) 2012-2013 Py-Hack developers
+Copyright (c) 2012-2013 HS developers
 See the file 'docs/Licence.md' for copying
 permissions
 """

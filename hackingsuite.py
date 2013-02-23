99e4b7ec5ed910216c5561b0fef7c475a58837e8
Drarqua-Storm <drarqua@hackgr.eu>
2013-02-23 14:03:01 +0200

    Fixed a crash error

--- hackingsuite.py
+++ hackingsuite.py
@@ -17,37 +17,40 @@ current_exploit = ""
 while True:
     options = raw_input("[hs~{"+current_exploit+"}]>> ")
     options = options.split()
-    if "help" == options[0].lower():
-        print interface._help
-    if "credits" == options[0].lower():
-        print interface._credits
-    if "search" == options[0].lower():
-        try:
-            options[1]
-        except:
-            pass
-        else:
-            print "[*]Searching for the script"
-            Get_file_path(options[1])
-    if "use" == options[0].lower():
-        try:
-            options[1]
-        except:
-            pass
-        else:
-            if check_file(options[1]) == False:
-                print "[-]Script not found"
-            elif check_file(options[1]) == True:
-                current_exploit = options[1].split("/")[-1]
-                curr_script = options[1]
-                script_options(options[1])
-                Option_dict = op.Options_Parser()
-                Option_dict.make_a_new_list(script_options(options[1]))
-    if "set" == options[0].lower():
-        Option_dict.store_new_function(options[1], options[2])
-        Option_dict.print_the_list()
-    if "attack" == options[0].lower():
-        Option_dict.exec_script(curr_script)
-    if "exit" == options[0].lower():
-        sys.exit()
+    try:
+        if "help" == options[0].lower():
+            print interface._help
+        if "credits" == options[0].lower():
+            print interface._credits
+        if "search" == options[0].lower():
+            try:
+                options[1]
+            except:
+                pass
+            else:
+                print "[*]Searching for the script"
+                Get_file_path(options[1])
+        if "use" == options[0].lower():
+            try:
+                options[1]
+            except:
+                pass
+            else:
+                if check_file(options[1]) == False:
+                    print "[-]Script not found"
+                elif check_file(options[1]) == True:
+                    current_exploit = options[1].split("/")[-1]
+                    curr_script = options[1]
+                    script_options(options[1])
+                    Option_dict = op.Options_Parser()
+                    Option_dict.make_a_new_list(script_options(options[1]))
+        if "set" == options[0].lower():
+            Option_dict.store_new_function(options[1], options[2])
+            Option_dict.print_the_list()
+        if "attack" == options[0].lower():
+            Option_dict.exec_script(curr_script)
+        if "exit" == options[0].lower():
+            sys.exit()
+    except IndexError:
+        pass
     

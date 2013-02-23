351980ed2f9099b66f56ccf9168384ea0f1ed4d2
Drarqua-Storm <drarqua@hackgr.eu>
2013-02-23 08:33:50 +0200

    Added a filter so it wont show .pyc files

--- core/script_handling/search_function.py
+++ core/script_handling/search_function.py
@@ -12,12 +12,15 @@ def Get_file_path(file_name):
     for path, dirname, fnames in os.walk("scripts"):
         for fname in fnames:
             if file_name.lower() in fname.lower():
-                if sys.platform.startswith('linux'):
-                    print ("[+]"+path+"/"+fname)
-                elif sys.platform.startswith('win'):
-                    full_path = path+"\\"+fname
-                    full_path = full_path.replace("\\", "/")
-                    print ("[+]"+full_path)
+                if ".pyc" in fname:
+                    pass
+                else:
+                    if sys.platform.startswith('linux'):
+                        print ("[+]"+path+"/"+fname)
+                    elif sys.platform.startswith('win'):
+                        full_path = path+"\\"+fname
+                        full_path = full_path.replace("\\", "/")
+                        print ("[+]"+full_path)
                     
 
 def check_file(file_name):
